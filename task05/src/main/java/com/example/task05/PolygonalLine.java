9dcea897387affeb2b246223b1b95360a383b662
Vadim Rudi <mrbrempy@gmail.com>
2026-09-07 22:53:11 +0500

    Задача 5

--- task05/src/main/java/com/example/task05/PolygonalLine.java
+++ task05/src/main/java/com/example/task05/PolygonalLine.java
@@ -1,17 +1,25 @@
 package com.example.task05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
-
+    private final List<Point> points = new ArrayList<>();
     /**
      * Устанавливает точки ломаной линии
      *
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        this.points.clear();
+        if (points != null) {
+            for (Point p: points) {
+                if (p != null) this.points.add(new Point(p.getX(), p.getY()));
+            }
+        }
     }
 
     /**
@@ -20,7 +28,7 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        if (point != null) this.points.add(new Point(point.getX(), point.getY()));
     }
 
     /**
@@ -30,7 +38,7 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        this.points.add(new Point(x, y));
     }
 
     /**
@@ -39,8 +47,11 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double length = 0.0;
+        for (int i = 0; i < points.size() - 1; i++) {
+            length += points.get(i).getLength(points.get(i + 1));
+        }
+        return length;
     }
 
 }
--- task05/src/main/java/com/example/task05/Task05Main.java
+++ task05/src/main/java/com/example/task05/Task05Main.java
@@ -2,6 +2,14 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
+        PolygonalLine line = new PolygonalLine();
+        line.addPoint(0, 0);
+        line.addPoint(1, 6);
+        line.addPoint(2, 7);
+        line.addPoint(8, 9);
+        line.addPoint(11, 99);
+        line.addPoint(15, 101);
 
+        System.out.println(line.getLength());
     }
 }
